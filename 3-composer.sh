2d754f4c29790f3cb3f59119d2e6463322c6837d
Paul Cuichan <ing.paul.cuixan@gmail.com>
2018-04-16 09:05:32 -0500

    Configuracion de docker

---
+++ 3-composer.sh
@@ -0,0 +1,7 @@
+
+#actualizo el kernel
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+#instalo pip
+sudo apt-get install -y python-pip
+#instalacion
+pip install docker-compose
